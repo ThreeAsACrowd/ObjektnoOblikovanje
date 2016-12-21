2628041e74fc860a85f3ac8a0608fd4a9c70f2d7
jurko5 <juraj.doncevic@outlook.com>
2016-12-21 11:36:04 +0100

    All windows created

---
+++ AccountingWPF/ViewModels/HomeViewModel.cs
@@ -0,0 +1,54 @@
+﻿using AccountingWPF.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AccountingWPF.ViewModels
+{
+	public class HomeViewModel
+	{
+		public Window ChildWindow { get; set; }
+
+		public HomeViewModel()
+		{
+
+		}
+
+		public void showReceiptMainWindow()
+		{
+			ChildWindow = new ReceiptMainWindow();
+			ChildWindow.ShowDialog();
+		}
+
+		public void showExpenditureMainWindow()
+		{
+			ChildWindow = new ExpenditureMainWindow();
+			ChildWindow.ShowDialog();
+		}
+
+		public void showIngoingInvoiceMainWindow()
+		{
+			ChildWindow = new IngoingInvoiceMainWindow();
+			ChildWindow.ShowDialog();
+		}
+
+		public void showOutgoingInvoiceMainWindow()
+		{
+			ChildWindow = new OutgoingInvoiceMainWindow();
+			ChildWindow.ShowDialog();
+		}
+
+		public void generateYearlyReport()
+		{
+
+		}
+
+		public void generateMonetaryFlowReport()
+		{
+
+		}
+	}
+}
