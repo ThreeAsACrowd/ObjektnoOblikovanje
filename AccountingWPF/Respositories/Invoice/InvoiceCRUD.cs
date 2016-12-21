a33f862b879d2266364d9e7f5e633b2b3b2b3648
MatejVukosav <bitbucketking>
2016-12-21 12:38:08 +0100

    uredeni folderi

---
+++ AccountingWPF/Respositories/Invoice/InvoiceCRUD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountingWPF.Models;
+
+namespace AccountingWPF.Respositories
+{
+    interface InvoiceCRUD
+    {
+
+        void Create(Invoice invoice);
+        void Delete(int id);
+
+        void Update(Invoice invoice);
+
+        Invoice GetById(int id);
+
+    }
+}
---
+++ AccountingWPF/Respositories/MonetaryFlow/ExpenditureRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountingWPF.Models;
+
+namespace AccountingWPF.Respositories
+{
+    class ExpenditureRepository : MonetaryFlowCRUD
+    {
+
+        public void Create(MonateryFlow monetaryFlow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MonateryFlow GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(MonateryFlow monetaryFlow)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
