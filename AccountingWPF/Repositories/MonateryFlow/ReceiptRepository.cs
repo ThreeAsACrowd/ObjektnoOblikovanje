a1c2cd781fdc70d3067ef14f825bd2e1affe9fab
MatejVukosav <vuki146@gmail.com>
2016-12-22 21:21:02 +0100

    napravljen receipt repozitorij create/delete/getById/update/getByUserId

--- AccountingWPF/Repositories/MonateryFlow/ReceiptRepository.cs
+++ AccountingWPF/Repositories/MonateryFlow/ReceiptRepository.cs
@@ -3,37 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using AccountingWPF.Factories;
 using AccountingWPF.Models;
+using AccountingWPF.nHibernateDb;
+using NHibernate;
+using NHibernate.Linq;
 
 namespace AccountingWPF.Repositories
 {
     public class ReceiptRepository : MonateryFlowCRUD
     {
 
-        public void Create(MonateryFlow monetaryFlow)
+        public void Create(MonateryFlow monateryFlow)
         {
-            Console.WriteLine("Create receipt");
+            using (var session = SessionManager.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.SaveOrUpdate(monateryFlow);
+                    transaction.Commit();
+                }
+            }
         }
 
         public void Delete(int id)
         {
-            Console.WriteLine("Delete receipt");
+            using (ISession session = SessionManager.OpenSession())
+            {
+
+                Receipt expenditure = session.Get<Receipt>(id);
+                if (expenditure == null)
+                {
+                    MessageBox.Show("Expenditure for given id does not exists");
+                    return;
+                }
+                session.Delete(expenditure);
+            }
         }
 
-        public void Update(MonateryFlow monetaryFlow)
+        public void Update(MonateryFlow monateryFlow)
         {
-            Console.WriteLine("Update receipt");
+            using (ISession session = SessionManager.OpenSession())
+            {
+                session.Update(monateryFlow);
+            }
         }
 
         public MonateryFlow GetById(int id)
         {
-            throw new NotImplementedException();
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Get<Receipt>(id);
+            }
         }
 
         public IList<Receipt> getByUserId(int userId)
         {
-            return Mock.getReceiptsByUserId(userId);
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Query<Receipt>()
+                     .Where(x => x.FK_UserId == userId)
+                     .ToList();
+            }
         }
     }
 }
