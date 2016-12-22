f674beb92a7518a815699e426643945363e24210
MatejVukosav <vuki146@gmail.com>
2016-12-22 23:06:39 +0100

    napravljen ingoingInvoiceRepozitorij create/delete/GetById/Update/getByUserId

--- AccountingWPF/Repositories/Invoice/IngoingInvoiceRepository.cs
+++ AccountingWPF/Repositories/Invoice/IngoingInvoiceRepository.cs
@@ -3,37 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using AccountingWPF.Factories;
 using AccountingWPF.Models;
+using AccountingWPF.nHibernateDb;
+using NHibernate;
+using NHibernate.Linq;
 
 namespace AccountingWPF.Repositories
 {
-    class IngoingInvoiceRepository : InvoiceCRUD
+    public class IngoingInvoiceRepository : InvoiceCRUD
     {
         public void Create(Invoice invoice)
         {
-            throw new NotImplementedException();
+            using (var session = SessionManager.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.SaveOrUpdate(invoice);
+                    transaction.Commit();
+                }
+            }
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (ISession session = SessionManager.OpenSession())
+            {
+
+                Invoice invoice = session.Get<Invoice>(id);
+                if (invoice == null)
+                {
+                    MessageBox.Show("Invoice for given id does not exists");
+                    return;
+                }
+                session.Delete(invoice);
+            }
         }
 
         public Invoice GetById(int id)
         {
-            throw new NotImplementedException();
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Get<Invoice>(id);
+            }
         }
 
         public void Update(Invoice invoice)
         {
-            throw new NotImplementedException();
+            using (ISession session = SessionManager.OpenSession())
+            {
+                session.Update(invoice);
+            }
         }
 
         public IList<IngoingInvoice> getByUserId(int userId)
         {
 
-            return Mock.getIngoingInvoicesByUserId(userId);
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Query<IngoingInvoice>()
+                     .Where(x => x.FK_UserId == userId)
+                     .ToList();
+            }
+
         }
     }
 }
