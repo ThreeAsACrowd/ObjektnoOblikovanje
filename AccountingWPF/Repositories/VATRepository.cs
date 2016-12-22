b621169b5178a9570d586d4b367fff4d75f12dc6
MatejVukosav <vuki146@gmail.com>
2016-12-22 22:41:53 +0100

    napravljen vat repozitorij create/delete/update/getById/getAll

--- AccountingWPF/Repositories/VATRepository.cs
+++ AccountingWPF/Repositories/VATRepository.cs
@@ -3,16 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using AccountingWPF.nHibernateDb;
+using NHibernate;
+using NHibernate.Linq;
 
 namespace AccountingWPF.Models
 {
-    class VATRepository
+    public class VATRepository
     {
 
-
-        public static IList<VAT> getAllVATs()
+        public void Create(VAT vat)
         {
-            return Factories.Mock.getAllVATs();
+            using (var session = SessionManager.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.SaveOrUpdate(vat);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+
+            using (ISession session = SessionManager.OpenSession())
+            {
+                VAT vat = session.Get<VAT>(id);
+                if (vat == null)
+                {
+                    MessageBox.Show("VAT for given id does not exists");
+                    return;
+                }
+                session.Delete(vat);
+            }
+
+        }
+
+        public void Update(VAT vat)
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+                session.Update(vat);
+            }
+        }
+
+        public VAT GetById(int id)
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+                return session.Get<VAT>(id);
+            }
+        }
+
+        public IList<VAT> getAll()
+        {
+            using (ISession session = SessionManager.OpenSession())
+            {
+
+                return session.Query<VAT>()
+                     .ToList();
+            }
         }
 
     }
