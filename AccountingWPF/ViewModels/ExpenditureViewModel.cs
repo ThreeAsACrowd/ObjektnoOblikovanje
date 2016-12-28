edd11a9e3d52fe33d642db57d8a025fe454aca99
nordlys2 <matea.cerovac1@gmail.com>
2016-12-28 23:40:32 +0100

    added delete mvvm logic for expenditures

--- AccountingWPF/ViewModels/ExpenditureViewModel.cs
+++ AccountingWPF/ViewModels/ExpenditureViewModel.cs
@@ -6,23 +6,54 @@ using System.Threading.Tasks;
 using DataRepository.Models;
 using DataRepository.Repositories;
 using AccountingWPF.BaseLib;
+using System.Diagnostics;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using AccountingWPF.Commands;
 
 namespace AccountingWPF.ViewModels
 {
     public class ExpenditureViewModel
     {
-        public IList<Expenditure> expenditures { get; set; }
+        public ObservableCollection<Expenditure> expenditures { get; set; }
         public IList<Vat> vats { get; set; }
         private IMonetaryFlowRepository<Expenditure> expenditureRepo { get; set; }
         private VatRepository vatRepo { get; set; }
 
+        public Expenditure selectedItem { get; set; }
+
+        public ICommand AddNewExpenditureCommand
+        {
+            get;
+            private set;
+        }
+
+        public ICommand DeleteExpenditureCommand
+        {
+            get;
+            private set;
+        }
+
+        public ICommand UpdateExpenditureCommand
+        {
+            get;
+            private set;
+        }
+
         public ExpenditureViewModel()
         {
+            IList<Expenditure> expendituresList;
             expenditureRepo = new ExpenditureRepository<Expenditure>();
 			vatRepo = new VatRepository();
 
-            expenditures = expenditureRepo.getByUserId(UserManager.CurrentUser.Id);
+            expendituresList = expenditureRepo.getByUserId(UserManager.CurrentUser.Id);
+            this.expenditures = new ObservableCollection<Expenditure>(expendituresList);
+
 			vats = vatRepo.getAll();
+
+            AddNewExpenditureCommand = new Command(this.AddNewExpenditure, this.CanExecuteAdd);
+            DeleteExpenditureCommand = new Command(this.DeleteExpenditure, this.CanExecuteDelete);
+            UpdateExpenditureCommand = new Command(this.UpdateExpenditure, this.CanExecuteUpdate);
 
         }
 
@@ -53,7 +84,31 @@ namespace AccountingWPF.ViewModels
 
         internal void AddNewExpenditure()
         {
-            throw new NotImplementedException();
+            //TODO IMPLEMENTATION
+            Debug.Assert(false, "Add new expenditure.");
+        }
+
+        internal void DeleteExpenditure()
+        {
+            if (this.selectedItem != null)
+            {
+
+                this.expenditureRepo.Delete(this.selectedItem.Id);
+                this.expenditures.Remove(this.selectedItem);
+
+            }
+            else
+            {
+                //TODO!
+                return;
+            }
+
+        }
+
+        internal void UpdateExpenditure()
+        {
+            //TODO IMPLEMENTATION
+            Debug.Assert(false, "Update invoice.");
         }
     }
 }
--- AccountingWPF/Views/ExpenditureMainWindow.xaml.cs
+++ AccountingWPF/Views/ExpenditureMainWindow.xaml.cs
@@ -29,44 +29,7 @@ namespace AccountingWPF.Views
             expenditureVM = new ExpenditureViewModel();
 			this.DataContext = expenditureVM;
 			this.dg_cbx_VAT.ItemsSource = expenditureVM.vats;
-            DeinitializeDataGrid();
-			
-		}
 
-        private void DeinitializeDataGrid() 
-        {
-            this.Delete_expenditure_button.IsEnabled = false;
-            this.Save_expenditure_button.IsEnabled = false;
-            this.dataGridExpenditure.IsEnabled = false;
-        }
-        private void InitializeDataGrid()
-        {
-            this.dataGridExpenditure.IsEnabled = true;
-            this.Delete_expenditure_button.IsEnabled = true;
-            this.Save_expenditure_button.IsEnabled = true;
-        }
-
-        private void Add_new_expenditure_Button_Click(object sender, RoutedEventArgs e)
-        {
-            InitializeDataGrid();
-            MessageBox.Show("Add");
-        }
-
-        private void Delete_expenditure_Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("delete");
-        }
-
-        private void Edit_expenditure_Button_Click(object sender, RoutedEventArgs e)
-        {
-            InitializeDataGrid();
-            MessageBox.Show("edit");
-        }
-
-        private void Save_ependiture_Button_Click(object sender, RoutedEventArgs e)
-        {
-            DeinitializeDataGrid();
-            MessageBox.Show("save");
         }
 	}
 }
