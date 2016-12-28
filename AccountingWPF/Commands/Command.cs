3acc73133f82a9a72130b6dd3ff9e238f3c7a60c
MatejVukosav <vuki146@gmail.com>
2016-12-28 02:38:42 +0100

    Univerzalna command klasa

---
+++ AccountingWPF/Commands/Command.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AccountingWPF.Commands
+{
+    public class Command : ICommand
+    {
+
+        private Action action;
+        private bool isEnabled;
+
+        public Command(Action action, bool isEnabled)
+        {
+            this.action = action;
+            this.isEnabled = isEnabled;
+
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add {
+                CommandManager.RequerySuggested += value;
+            }
+            remove {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return isEnabled;
+        }
+
+
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
