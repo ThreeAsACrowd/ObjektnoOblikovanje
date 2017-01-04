5e5eb85c08176158058ac188c1f95f4581220429
jurko5 <juraj.doncevic@outlook.com>
2017-01-04 15:26:50 +0100

    date validation is now functional

--- AccountingWeb/App_Start/BundleConfig.cs
+++ AccountingWeb/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace AccountingWeb
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+			//Globalize bundle
+			bundles.Add(new ScriptBundle("~/bundles/globalize").Include(
+			"~/Scripts/globalize.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
